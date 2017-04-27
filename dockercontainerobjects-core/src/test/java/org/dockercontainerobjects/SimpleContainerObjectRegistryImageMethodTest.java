ecd6c25831e89efe86983338304625d19dc67a1b
Ramon Rivas <rivasdiaz@gmail.com>
2017-04-27 14:03:52 -0400

    test case for selecting image by calling a container object method

---
+++ dockercontainerobjects-core/src/test/java/org/dockercontainerobjects/SimpleContainerObjectRegistryImageMethodTest.java
@@ -0,0 +1,57 @@
+package org.dockercontainerobjects;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.dockercontainerobjects.annotations.RegistryImage;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitPlatform.class)
+@DisplayName("Simple container object tests")
+@Tag("docker")
+public class SimpleContainerObjectRegistryImageMethodTest extends ContainerObjectManagerBasedTest {
+
+    private static SimpleContainer container;
+
+    @BeforeAll
+    static void createContainer() {
+        container = manager.create(SimpleContainer.class);
+    }
+
+    @AfterAll
+    static void destroyContainer() {
+        manager.destroy(container);
+    }
+
+    @Test
+    @DisplayName("A container object, after created, should return status STARTED")
+    void containerStatusStarted() {
+        assertEquals(ContainerObjectsManager.ContainerStatus.STARTED, manager.getContainerStatus(container));
+    }
+
+    @Test
+    @DisplayName("A container object, after created, should have non-null id")
+    void containerIdNotNull() {
+        assertNotNull(manager.getContainerId(container));
+    }
+
+    @Test
+    @DisplayName("A container object, after created, should have a valid address")
+    void containerValidAddress() {
+        assertNotNull(manager.getContainerAddress(container));
+    }
+
+    public static class SimpleContainer {
+
+        @RegistryImage
+        protected static String image() {
+            return "tomcat:jre8";
+        }
+    }
+}
