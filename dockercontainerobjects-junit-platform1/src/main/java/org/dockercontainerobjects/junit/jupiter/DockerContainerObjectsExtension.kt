ee9b2e6e408205abcb4ee19e1dd61817bdc6e32b
Ramon Rivas <rivasdiaz@gmail.com>
2017-08-02 02:18:53 -0400

    update to JUnit 5.0.0-RC2 and align to their breaking changes. update Gradle, Docker-Java and other libraries to latest version

--- dockercontainerobjects-junit-platform1/src/main/java/org/dockercontainerobjects/junit/jupiter/DockerContainerObjectsExtension.kt
+++ dockercontainerobjects-junit-platform1/src/main/java/org/dockercontainerobjects/junit/jupiter/DockerContainerObjectsExtension.kt
@@ -6,29 +6,28 @@ import org.junit.jupiter.api.extension.AfterAllCallback
 import org.junit.jupiter.api.extension.AfterEachCallback
 import org.junit.jupiter.api.extension.BeforeAllCallback
 import org.junit.jupiter.api.extension.BeforeEachCallback
-import org.junit.jupiter.api.extension.ContainerExtensionContext
-import org.junit.jupiter.api.extension.TestExtensionContext
+import org.junit.jupiter.api.extension.ExtensionContext
 
 class DockerContainerObjectsExtension: BeforeAllCallback, BeforeEachCallback, AfterEachCallback, AfterAllCallback {
 
     lateinit var enhancer: ContainerObjectsClassEnhancer
         private set
 
-    override fun beforeAll(context: ContainerExtensionContext) {
+    override fun beforeAll(context: ExtensionContext) {
         enhancer = ContainerObjectsEnvironmentFactory.newEnvironment().enhancer
-        context.testClass.ifPresent(enhancer::setupClass)
+        enhancer.setupClass(context.requiredTestClass)
     }
 
-    override fun beforeEach(context: TestExtensionContext) {
-        enhancer.setupInstance(context.testInstance)
+    override fun beforeEach(context: ExtensionContext) {
+        enhancer.setupInstance(context.requiredTestInstance)
     }
 
-    override fun afterEach(context: TestExtensionContext) {
-        enhancer.teardownInstance(context.testInstance)
+    override fun afterEach(context: ExtensionContext) {
+        enhancer.teardownInstance(context.requiredTestInstance)
     }
 
-    override fun afterAll(context: ContainerExtensionContext) {
-        context.testClass.ifPresent(enhancer::teardownClass)
+    override fun afterAll(context: ExtensionContext) {
+        enhancer.teardownClass(context.requiredTestClass)
         enhancer.close()
     }
 }
